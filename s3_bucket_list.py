585bdc98efe065a25ca32fe2cc2faa3bf2b919e2
carnal0wnage <chris@carnal0wnage.com>
2017-05-31 11:09:09 -0400

    test file

---
+++ s3_bucket_list.py
@@ -0,0 +1,18 @@
+import boto3
+import botocore
+
+import json
+import urllib
+import logging
+import sys,os
+import pprint
+
+pp = pprint.PrettyPrinter(indent=5, width=80)
+
+from s3.s3 import *
+
+#cgates pythons3
+AWS_ACCESS_KEY_ID = ''
+AWS_SECRET_ACCESS_KEY =''
+
+get_s3bucket_policy(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY,'myfuckingbucket')
