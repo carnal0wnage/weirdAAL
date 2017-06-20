2f4997cf77a12383cbba0fb04bc7b0c06dce20f6
carnal0wnage <chris@carnal0wnage.com>
2017-06-20 12:50:04 -0400

    buckets and contents

---
+++ s3_list_buckets_and_contents.py
@@ -0,0 +1,21 @@
+import boto3
+import botocore
+
+import json
+import urllib
+import logging
+import sys,os
+import pprint
+
+pp = pprint.PrettyPrinter(indent=5, width=80)
+
+from s3.s3 import *
+
+#insert AWS key, will figure out how to pull this in from a single file for all scripts
+
+#AWS_ACCESS_KEY_ID = ''
+#AWS_SECRET_ACCESS_KEY =''
+
+
+
+get_s3objects_for_account_detailed(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY)
