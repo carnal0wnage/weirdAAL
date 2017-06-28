a4bcc94d991830b27ff9b0ea8bbcd88ca67fbe81
carnal0wnage <chris@carnal0wnage.com>
2017-06-27 22:00:08 -0400

    rds cleanup

--- libs/rds.py
+++ libs/rds.py
@@ -26,6 +26,3 @@ def describe_db_instances(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY):
 
 	except botocore.exceptions.ClientError as e:
 		print e
-
-
-describe_db_instances(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY)
