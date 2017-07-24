ce188e8224a14e4d9c4af6d266598c0af408fef7
cmc <cmc@uber.com>
2017-07-24 14:22:36 -0700

    + lambda refactor

--- lambda_list_functions_and_mappings.py
+++ lambda_list_functions_and_mappings.py
@@ -1,18 +1,6 @@
 '''
 This file is used to list lambda functions and event mappings
 '''
-
-import boto3
-import botocore
-
-import json
-import urllib
-import logging
-import sys,os
-import pprint
-
-pp = pprint.PrettyPrinter(indent=5, width=80)
-
 from libs.aws_lambda import *
 from config import AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
 
