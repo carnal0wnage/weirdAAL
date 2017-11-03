681e899fd02893b3698278bdcc95038499fb9f66
cktricky <cktricky@gmail.com>
2017-11-02 21:52:53 -0400

    getting familiar with options

--- weirdAAL.py
+++ weirdAAL.py
@@ -11,4 +11,9 @@ import argparse
 
 import argparse
 parser = argparse.ArgumentParser()
-parser.parse_args()
+parser.add_argument("-v", "--verbosity", help="increase output verbosity",
+action="store_true")
+args = parser.parse_args()
+
+if (args.verbosity):
+    print("Verbosity is enabled")
