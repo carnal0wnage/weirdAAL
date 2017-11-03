dd9b53234afa3131de16f566e39fe1dfc69e2b11
cktricky <cktricky@gmail.com>
2017-11-02 21:27:04 -0400

    testing imports after adding a requirements.txt file which is akin to a Gemfile or package.json

--- weirdAAL.py
+++ weirdAAL.py
@@ -2,6 +2,9 @@
 # Things we want to figure out
 # 1) Is your key active?
 # 2) If active, can you read monitoring configs, can you write?
-# 3) Okay, you can read monitoring configs. We recommend things to avoid. Want to go further? Use write access to disable (if applicable) 
+# 3) Okay, you can read monitoring configs. We recommend things to avoid. Want to go further? Use write access to disable (if applicable)
 # 4) Don't want to do anything with monitoring? That's fine, let's guide you through figuring out what your access looks like
 # 5) Help with a printout of options from this point forward
+
+import boto3
+import argparse
