6d69e6b02455060229de820f270a2873b519112a
carnal0wnage <chris@carnal0wnage.com>
2018-04-04 22:12:32 -0400

    initital db code

---
+++ create_dbs.py
@@ -0,0 +1,12 @@
+import sqlite3
+from sqlite3 import Error
+
+from  libs.sql import *
+
+
+#create some tables to stick data in
+
+if __name__ == "__main__":
+    db_name = "weirdAAL.db"
+    create_awskey_table(db_name, "AWSKey")
+    create_recon_table(db_name, "recon")
