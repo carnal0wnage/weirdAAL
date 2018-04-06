f92a7c51a312e58a1b055a60c489fae6c7aa0176
cktricky <cktricky@gmail.com>
2018-04-05 14:19:35 -1000

    removing testing method that is unnecessary

--- libs/brute.py
+++ libs/brute.py
@@ -21,9 +21,6 @@ def get_accountid(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY):
 #    print username
 #    return username
 
-def step_app():
-    print('hiiiiiii')
-
 def check_root_account(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY):
     client = boto3.client('iam', aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
 
