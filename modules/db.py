f2d3f385d484b6db6bd89f7ad82724813db37813
carnal0wnage <chris@carnal0wnage.com>
2018-04-07 14:01:06 -0400

    move show services to modules/db.py

---
+++ modules/db.py
@@ -0,0 +1,15 @@
+import sqlite3
+from sqlite3 import Error
+
+from  libs.sql import *
+
+from config import AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
+
+
+#if __name__ == "__main__":
+def step_show_services_by_key():
+    db_name = "weirdAAL.db"
+    results = search_recon_by_key(db_name,AWS_ACCESS_KEY_ID)
+    print("Services enumerated for {}".format(AWS_ACCESS_KEY_ID))
+    for result in results:
+    	print("{}:{}".format(result[0],result[1]))
