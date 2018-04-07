76db8c3aea70ad82fc795c0c6852902e44f873a7
carnal0wnage <chris@carnal0wnage.com>
2018-04-07 19:46:38 -0400

    lambda

--- emr_list_clusters.py
+++
@@ -1,9 +0,0 @@
-'''
-This file is used to perform some EMR actions
-'''
-from libs.emr import *
-from config import AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
-
-
-list_clusters(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY)
-list_security_configurations(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY)
--- lambda_list_functions_and_mappings.py
+++
@@ -1,8 +0,0 @@
-'''
-This file is used to list lambda functions and event mappings
-'''
-from libs.aws_lambda import *
-from config import AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
-
-list_functions(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY)
-list_event_source_mappings(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY)
