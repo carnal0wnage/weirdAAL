6428dca3af8394a386d20ae2345d969adb4299ed
cktricky <cktricky@gmail.com>
2018-04-19 22:14:31 -0400

    little touchup

--- modules/translate.py
+++ modules/translate.py
@@ -5,8 +5,8 @@ Translate module
 
 from libs.translate import *
 
-def module_translate_translate_text(text):
+def module_translate_translate_text(*text):
 	'''
 	translate text ==> text, source_language, target_language
 	'''
-	translate_text(text[0], 'en', 'es')
+	translate_text(text[0][0], text[0][1], text[0][2])
