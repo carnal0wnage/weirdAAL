15c971645355396bf2ed94b300d010c0e580de35
carnal0wnage <chris@carnal0wnage.com>
2020-05-18 16:33:33 -0400

    code cleanup

--- libs/aws/s3.py
+++ libs/aws/s3.py
@@ -19,7 +19,7 @@ AWS_ACCESS_KEY_ID = credentials.access_key
 AWS_SECRET_ACCESS_KEY = credentials.secret_key
 
 # from http://docs.aws.amazon.com/general/latest/gr/rande.html
-regions = ['us-east-1', 'us-east-2', 'us-west-1', 'us-west-2', 'af-south-1', 'ap-east-1', 'ap-northeast-1', 'ap-northeast-2', 'ap-northeast-3', 'ap-south-1', 'ap-southeast-1', 'ap-southeast-2', 'ca-central-1', 'cn-north-1', 'cn-northwest-1', 'eu-central-1', 'eu-west-1', 'eu-west-2', 'eu-west-3', 'eu-south-1', 'eu-north-1', 'me-south-1', 'sa-east-1', 'us-gov-west-1', 'us-gov-east-1']
+regions = ['us-east-1', 'us-east-2', 'us-west-1', 'us-west-2', 'ca-central-1', 'ap-south-1', 'ap-northeast-1', 'ap-northeast-2', 'ap-northeast-3', 'ap-southeast-1', 'ap-southeast-2', 'cn-north-1', 'cn-northwest-1', 'eu-central-1', 'eu-west-1', 'eu-west-2', 'eu-west-3', 'sa-east-1']
 
 region = 'us-east-1'
 
@@ -176,7 +176,7 @@ def s3_get_objects_for_account():
     try:
         client = boto3.resource('s3', region_name=region)
 
-        print('#### Trying to list s3 bucketsfor {} ####\n '.format(AWS_ACCESS_KEY_ID))
+        print('#### Trying to list s3 buckets for {} ####\n '.format(AWS_ACCESS_KEY_ID))
         for bucket in client.buckets.all():
             print(bucket.name)
 
@@ -202,7 +202,7 @@ def s3_get_objects_for_account_detailed():
     try:
         client = boto3.resource('s3', region_name=region)
 
-        print('#### Trying to list s3 bucketsfor {} ####\n '.format(AWS_ACCESS_KEY_ID))
+        print('#### Trying to list s3 buckets for {} ####\n '.format(AWS_ACCESS_KEY_ID))
         for bucket in client.buckets.all():
             print(bucket.name)
             s3_get_bucket_policy(bucket.name)
